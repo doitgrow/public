7fdbfaae479ec5a9748a3e29d6283dcfeeffd586
DoITGrow <doitgrow@gmail.com>
2024-08-10 23:20:14 +0900

    dfdf

--- kubernetes/kubeadam-kubelet-kubectl-install.sh
+++ kubernetes/kubeadam-kubelet-kubectl-install.sh
@@ -4,10 +4,12 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
 # 2. 구글 클라우드의 공개 사이닝 키를 다운로드 한다.
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
 
 # 3. 쿠버네티스 apt 리포지터리를 추가한다.
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
 
 # 4. apt 패키지 색인을 업데이트하고, kubelet, kubeadm, kubectl을 설치하고 해당 버전을 고정한다.
 sudo apt-get update
